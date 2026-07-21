18f4e692a6a62af9e9fca0d71d404e889e28d489
Oleg <ikslprint@gmail.com>
2026-07-21 13:40:14 +0300

    rework useSeoMeta

--- app/app.vue
+++ app/app.vue
@@ -5,3 +5,15 @@
     </NuxtLayout>
   </UApp>
 </template>
+
+
+<script setup>
+useSeoMeta({
+  ogTitle: 'Horeca Da - сучасна ресторанна бухгалтерія',
+  ogDescription: 'Професійний фінансовий супровід, аудит та автоматизація для закладів Horeca.',
+  ogImage: 'https://da-horeca.vercel.app/og-image.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
+
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,12 +9,3 @@
     <LandingContactFormSection id="contact" />
   </main>
 </template>
-
-<script setup>
-useSeoMeta({
-  ogTitle: 'Horeca Da - сучасна ресторанна бухгалтерія',
-  ogDescription: 'Професійний фінансовий супровід, аудит та автоматизація для закладів Horeca.',
-  ogImage: 'https://da-horeca.vercel.app/og-image.png',
-  twitterCard: 'summary_large_image',
-})
-</script>
