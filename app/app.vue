5a630dc10e0453e6027ae139f5def73564a7fb9a
Oleg <ikslprint@gmail.com>
2026-07-23 21:17:03 +0300

    rework og image

--- app/app.vue
+++ app/app.vue
@@ -12,6 +12,8 @@ useSeoMeta({
   ogTitle: 'Horeca Da - сучасна ресторанна бухгалтерія',
   ogDescription: 'Забираємо рутину з накладними та інвентаризаціями. Допомагаємо бачити реальний чистий прибуток і контролювати фудкост.',
   ogImage: 'https://www.horeca-da.com.ua/og-image.png',
+  ogSiteName: 'Horeca Da',
+  ogUrl: 'https://www.horeca-da.com.ua/',
   twitterCard: 'summary_large_image',
 })
 </script>
