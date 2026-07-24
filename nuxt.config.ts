876073ee15b9b265bc23f5527b9f1d8aa4f483cd
Oleg <ikslprint@gmail.com>
2026-07-24 15:05:07 +0300

    fix studio repo config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,14 @@ export default defineNuxtConfig({
       ]
     }
   },
+  studio: {
+    repository: {
+      provider: 'github',
+      owner: 'VlasiukOleg',
+      repo: 'da-horeca',
+      branch: 'main'
+    }
+  },
   nitro: {
     prerender: {
       routes: ['/'],
