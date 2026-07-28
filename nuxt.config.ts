7ddd61100ff9aef78c180fe7f7a70483a712a080
Oleg <ikslprint@gmail.com>
2026-07-28 11:09:21 +0300

    fix studio-env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,58 +1,52 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/google-fonts', '@nuxt/content', 'nuxt-studio'],
-  css: ['~/assets/css/main.css'],
+  modules: ["@nuxt/ui", "@nuxtjs/google-fonts", "@nuxt/content", "nuxt-studio"],
+  css: ["~/assets/css/main.css"],
+
+  // Явне прокидання змінних для обходу бага модуля
   runtimeConfig: {
     telegramBotToken: "",
     telegramChatId: "",
-    studio: {
-      auth: {
-        github: {
-          clientId: "",
-          clientSecret: ""
-        }
-      }
-    }
+  },
+  studio: {
+    repository: {
+      provider: "github",
+      owner: "VlasiukOleg",
+      repo: "da-horeca",
+      branch: "main",
+    },
   },
   app: {
     head: {
-      title: 'HoReCa Da — Сучасна ресторанна бухгалтерія',
+      title: "HoReCa Da — Сучасна ресторанна бухгалтерія",
       meta: [
-        { name: 'description', content: 'Будуємо управлінський облік, який підлаштовується під ваш бізнес, щоб ви могли сфокусуватися на гостях, смаку та розвитку закладу.' }
-      ]
-    }
-  },
-studio: {
-    repository: {
-      provider: 'github',
-      owner: 'VlasiukOleg',
-      repo: 'da-horeca',
-      branch: 'main'
+        {
+          name: "description",
+          content:
+            "Будуємо управлінський облік, який підлаштовується під ваш бізнес, щоб ви могли сфокусуватися на гостях, смаку та розвитку закладу.",
+        },
+      ],
     },
-    // Додай цей блок автентифікації прямо у файл:
-    auth: {
-      github: {
-        clientId: process.env.NUXT_STUDIO_AUTH_GITHUB_CLIENT_ID,
-        clientSecret: process.env.NUXT_STUDIO_AUTH_GITHUB_CLIENT_SECRET
-      }
-    }
   },
-  nitro: {
-    prerender: {
-      routes: ['/'],
-      crawlLinks: true
-    }
-  },
+
+  // nitro: {
+  //   prerender: {
+  //     routes: ["/"],
+  //     crawlLinks: true,
+  //   },
+  // },
+
   googleFonts: {
     families: {
-      Nunito: [400, 700, 900], // 900 — для тих самих жирних заголовків
+      Nunito: [400, 700, 900],
     },
-    display: 'swap',
-    download: true // Завантажує шрифт локально при збірці (краще для швидкості)
+    display: "swap",
+    download: true,
   },
+
   features: {
-    inlineStyles: true
-  }
+    inlineStyles: true,
+  },
 });
